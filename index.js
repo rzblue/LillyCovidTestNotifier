5986e406f755ece7503bfcf99caca9a6757f25ff
Ryan Blue <ryanzblue@gmail.com>
2020-03-31 17:57:22 -0400

    Add some error handling logic

--- index.js
+++ index.js
@@ -3,9 +3,8 @@ let conf = require('./conf.json');
 var twilio = require('twilio');
 var client = new twilio(conf.twilio.account_sid, conf.twilio.auth_token);
 let prevResults = {};
-
+loopFunc();
 let interval = setInterval(loopFunc, conf.frequency * 1000);
-
 /**
  * Returns true when previous result is different from current result
  */
@@ -14,7 +13,7 @@ function compareResults(previous, current) {
         return false
     } else if(isEmpty(current.patient.test_results)) {
         return false;
-      } else return current.patient.test_results !== previous.patient.test_results;
+    } else return current.patient.test_results !== previous.patient.test_results;
 }
 
 /**
@@ -33,13 +32,17 @@ function isEmpty(obj) {
  */
 function loopFunc() {
     axios.post(conf.apiURL, conf.api).then((res) => {
+        if(res.data.hasOwnProperty('error')) {
+            console.log("Server returned error: %O", res.data.error);
+            process.exit();
+        }
         console.log("Result update: %O", res.data.patient.test_results);
         if(compareResults(prevResults, res.data)) {
             console.log("New result found, sending text.")
             sendUpdateText();
         }
         prevResults = res.data;
-    })
+    });
 }
 
 /**
